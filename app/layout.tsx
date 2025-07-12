b9a6be82b35330e24c73c63b67b8d32bd7920b19
Callen Flynn <not.skittle.or.beluga@gmail.com>
2025-07-12 01:52:07 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Concord SMP',
   description: 'A minecraft server build around community',
+    icons: {
+    icon: '/image.png', // This points to your image.png in the public directory
+  },
 }
 
 export default function RootLayout({
