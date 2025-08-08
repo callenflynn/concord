df2755acc3c0c8a1a58cc45e56a5d1f5e37554f3
Callen Flynn <not.skittle.or.beluga@gmail.com>
2025-08-07 22:04:19 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -464,7 +464,7 @@ const getVersionInfo = () => {
                   <div className="flex-1">
                     <h3 className="text-lg font-medium text-slate-700 mb-2">preconfigured client installer (recommended, optional)</h3>
                     <p className="text-slate-500 mb-3 font-light">
-                      Installs Java 21 JDK, Fabric 1.21.7, and all the mods and shaders. Recommended for those who have not yet modded minecraft or do not know how. 
+                      Installs Java 21 JDK, Fabric 1.21.7, and all the mods and shaders. Recommended for those who have not yet modded minecraft or do not know how. After ran, please go to your Minecraft launcher and create a new instance with the 1.21.7 FABRIC version.
                     </p>
                     <p className="text-slate-500 mb-3 font-light">
                       Also includes the files in modrinth format, and the modrinth installer for an easier install and smoother experience.
